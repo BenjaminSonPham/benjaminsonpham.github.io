ae7e5c5c6b5aff0bea6a8ac69cc096625ffb9b70
benjaminsonpham <benjaminsonpham@gmail.com>
2020-04-09 11:53:57 -0700

    Added aliases

--- scripts/snow_this_machine.sh
+++ scripts/snow_this_machine.sh
@@ -1,5 +1,25 @@
 #!/bin/bash 
 
+#configure aliases
+
+touch .bashrc.aliases
+printf "ls='ls -c' \nip='ip -c' \ngrep='grep -color' \nls='ls --color=auto'\n" >> .bashrc.aliases
+
+printf "source .basrc.aliases" > .bashrc
+
+#enable colorscheme for this user. use $USER to make things modular
 
 
+#Check linux distro family
 
+
+#install the following packages to the server
+vim 
+mlocate
+
+os-family="SOMETHING"
+if [-f /etc/os-release]; then 
+	os-family="Debian"
+
+if [-f /etc/redhat-release]; then 
+	os-family="Redhat"
